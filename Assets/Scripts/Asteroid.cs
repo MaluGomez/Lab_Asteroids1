93d86dceda6a73754f161383cb4476244a74dde9
MaluGomez <malugomezr.mgr@gmail.com>
2020-10-24 17:33:50 -0500

    Asteroids

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed;
+
+    public GameObject[] subAsteroids;
+
+    public int numberOfAsteroids;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.drag = 0;
+        rb.angularDrag = 0;
+
+        rb.velocity = new Vector3(
+            Random.Range(-1f,1f),
+            Random.Range(-1f,1f),
+            0
+        ).normalized*speed;
+        rb.angularVelocity = Random.Range (-50f, 50f);
+    }
+
+    private bool isDestroying = false;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (isDestroying)
+        {
+            return;
+        }
+        if (col.CompareTag("Bullet"))
+        {
+            isDestroying = true;
+            Destroy(gameObject);
+            Destroy(col.gameObject);
+            for (var i = 0; i < numberOfAsteroids; i++)
+            {
+                Instantiate(
+                    subAsteroids[Random.Range(0, subAsteroids.Length)],
+                    transform.position,
+                    Quaternion.identity
+                );
+            }
+        }
+        if (col.CompareTag ("Player")) {
+            var asteroids = FindObjectsOfType<Asteroid>();
+            for(var i = 0; i < asteroids.Length; i++) {
+                Destroy(asteroids[i].gameObject);
+            }
+            col.GetComponent<Player>().Lose();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float accerleration;
+
+    public float maxSpeed;
+
+    public float drag;
+
+    public float angularSpeed;
+
+    public float shootRate = 0.5f;
+
+    public float offsetBullet;
+
+    public GameObject bulletPrefab;
+
+    public Transform bulletSpawner;
+
+    private Rigidbody2D rb;
+
+    private float vertical;
+    private float horizontal;
+    private bool shooting;
+    private bool canShoot = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.drag = drag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = InputManager.Vertical;
+        horizontal = InputManager.Horizontal;
+        shooting = InputManager.Fire;
+
+        Rotate();
+        Shoot();
+    }
+
+    private void FixedUpdate()
+    {
+        var fowardMotor = Mathf.Clamp(vertical,0f,1f);
+        rb.AddForce(transform.up * accerleration * fowardMotor);
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+    public void Lose () {
+        rb.velocity = Vector3.zero;
+        transform.position = Vector3.zero;
+        SceneManager.LoadScene(0);
+    }
+    private void Rotate()
+    {
+        if (horizontal == 0)
+        {
+            return;
+        }
+        transform.Rotate(0,0,-angularSpeed *horizontal *Time.deltaTime);
+    }
+    private void Shoot () {
+        if (shooting && canShoot)
+        {
+            StartCoroutine(FireRate());
+
+        }
+    }
+    private IEnumerator FireRate () {
+        canShoot = false;
+        var pos = transform.up * offsetBullet + transform.position;
+        var bullet = Instantiate (
+            bulletPrefab,
+            pos,
+            transform.rotation
+        );
+        Destroy (bullet, 5);
+        yield return new WaitForSeconds (shootRate);
+        canShoot = true;
+    }
+}
